a540dbd1a0a80c5d3118d30097558b76a056420f
Anton <ryabchuk.anton@yandex.ru>
2020-11-04 20:22:25 +0300

    change workflow

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (UserViewSet, send_confirmation_code, obtain_token,
-                    CategoryViewSet, GenreViewSet, TitleViewSet, ReviewViewSet, CommentViewSet)
+                    CategoryViewSet, GenreViewSet, TitleViewSet, ReviewViewSet,
+                    CommentViewSet)
 
 
 router = DefaultRouter()
@@ -11,7 +12,7 @@ router.register(r'genres', GenreViewSet)
 router.register(r'titles', TitleViewSet)
 router.register(r'titles/(?P<title_id>\d+)/reviews',
                 ReviewViewSet, basename='Review')
-router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+router.register(r'titles/(?P < title_id >\d+)/reviews/(?P < review_id >\d+)/comments',
                 CommentViewSet, basename='Comment')
 
 auth_patterns = [
